cf99ee30dca236e786aa2fed4614e50196991eb2
manuelrojas <manuel.rojas.21@gmail.com>
2022-01-30 10:53:13 -0600

    Adding mobile tweaks

--- pages/components/banner.tsx
+++ pages/components/banner.tsx
@@ -1,7 +1,7 @@
 
 export default function Banner() {
     return (
-      <div className=" bg-break.away-blue p-10 mt-10">
+      <div className=" bg-break.away-blue px-20 py-10 mt-10">
             <h1 className=" text-white text-3xl text-center">¡Atrévete a vivir el ride que siempre soñaste!</h1>
       </div>
     )
