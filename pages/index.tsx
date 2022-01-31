7d870fff85c250f2caa6ee504bfcf302e486f991
manuelrojas <manuel.rojas.21@gmail.com>
2022-01-31 15:23:02 -0600

    Adding banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Main from './components/main'
 import List from './components/list'
 import NewsletterSubscribe from './components/suscribe'
 import Banner from './components/banner'
+import Footer from './components/footer'
+import Fade from 'react-reveal/Fade'
 
 export default function Home() {
   return (
@@ -20,6 +22,8 @@ export default function Home() {
       <List />
       <Banner />
       <NewsletterSubscribe />
+      <Footer />
+
     </div>
   )
 }
